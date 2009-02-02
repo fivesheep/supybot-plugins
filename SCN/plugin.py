db97f9de9cb6739b7db047220d8ebd30cb7f26ed
Young Ng <fivesheep@gmail.com>
2009-02-02 11:56:47 +0000

    1. add help for the commands. 2. don't read the remote data at one time, but just the needed info. 3. provide a --limit option for the command pre to limit the num of returning data.

--- SCN/plugin.py
+++ SCN/plugin.py
@@ -60,29 +60,37 @@ class SCN(callbacks.Plugin):
         req=urllib2.Request(url,None,{'User-agent': 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2)'})
         return urllib2.urlopen(req)
 
-    def _search(self,qstrs,cat=None):
-        url=SCN.BASE_URL+'+'.join(qstrs)
-        if cat != None and cat in SCN.CATALOGS:
-            url=url+'&cat='+cat
-        resp=self._urlopen(url)
-        data=resp.read()
-        resp.close()
-        iter=SCN.DATA_PATTERN.finditer(data)
+    def _search(self,qstrs,cat=None,limit=5):
+        url=SCN.BASE_URL
         results=[]
-        for i in iter:
-            if len(results) >= SCN.NUM_OF_RESULTS:
+        if len(qstrs)!=0:
+            url+='+'.join(qstrs)
+        if cat != None and cat in SCN.CATALOGS:
+            url+='&cat='+cat
+        resp=self._urlopen(url)
+        while(1):
+            line=resp.readline()
+            if line=='':
+                # reach the end of the remote data.
+                break    
+            if line.startswith('<table>') and line.find('</table>') == -1:
+                # begin to read releases data
+                for i in range(limit):
+                    release_info=resp.readline.rstrip('\n')
+                    m=SCN.DATA_PATTERN.match(release_info)
+                    if m:
+                        result={}
+                        result['date']=m.group(1)
+                        result['catalog']=m.group(2)
+                        result['name']=m.group(3).split(' ')[0]
+                        if m.group(5)!=None and len(m.group(5))>5:
+                            result['nfo']=SCN.NFO_URL_PATTERN.findall(m.group(5))[0]
+                        else:
+                            result['nfo']=''
+                        results.append(result)
                 break
-
-            result={}
-            result['date']=i.group(1)
-            result['catalog']=i.group(2)
-            result['name']=i.group(3).split(' ')[0]
-            if i.group(5)!=None and len(i.group(5))>5:
-                result['nfo']=SCN.NFO_URL_PATTERN.findall(i.group(5))[0]
-            else:
-                result['nfo']=''
-            results.append(result)
-
+                                                
+        resp.close()
         return results
 
     def _print_result(self,irc,results):
@@ -90,66 +98,121 @@ class SCN(callbacks.Plugin):
             irc.reply('Sorry, Nothing Found!')
         else:
             for i in results:
-                irc.reply('[%s] %s %s [ NFO: %s ]'%(i['date'],i['catalog'],i['name'],i['nfo']))
-
+                irc.reply('[%s] %s %s | NFO: %s'%(i['date'],i['catalog'],i['name'],i['nfo']))
 
     def pre(self,irc,msg,args,options,qstrs):
+        """[OPTION]... [keyword]...
+        
+        Search Scene releases via pre.scnsrc.net. Searching options including: 
+        --limit the max num of the returning results, it's 5 by default; 
+        --cat the catalog of the releases, it's value could be one of the 
+              following: xvid,dvdr,x264,bluray,tv,xxx, mp3, 0day, apps, games, 
+                       console, handheld, mvid, ebook, dox, psp, nds, pda..
+        """
         cat=None
         options=dict(options)
         if options.has_key('cat'):
             cat=options['cat'].upper()
+        if options.has_key('limit'):
+            limit=options['limit']
+        else:
+            limit=SCN.NUM_OF_RESULTS
         
-        results=self._search(qstrs,cat)
+        results=self._search(qstrs,cat,limit)
 
         self._print_result(irc,results)
 
-    pre=wrap(pre,[getopts({'cat':'something'}),many('something')])
+    pre=wrap(pre,[getopts({'cat':'something', 'limit':'int'}),any('something')])
 
-    def zd(self,irc,msg,args,qstrs):
+    def zday(self,irc,msg,args,qstrs):
+        """[keyword]...
+        
+        Search for 0day releases.
+        """
         results=self._search(qstrs,'0DAY')
         self._print_result(irc,results)
-    zd=wrap(zd,[many('something')])
+    zday=wrap(zd,[any('something')])
 
     def apps(self,irc,msg,args,qstrs):
+        """[keyword]...
+        
+        Search for released apps.
+        """
         results=self._search(qstrs,'APPS')
         self._print_result(irc,results)
-    apps=wrap(apps,[many('something')])
+    apps=wrap(apps,[any('something')])
 
     def movie(self,irc,msg,args,qstrs):
+        """[keyword]...
+        
+        Search for released movies(xvid).
+        """
         results=self._search(qstrs,'XVID')
         self._print_result(irc,results)
-    movie=wrap(movie,[many('something')])
+    movie=wrap(movie,[any('something')])
     
     def mp3(self,irc,msg,args,qstrs):
+        """[keyword]...
+        
+        Search for released mp3s.
+        """
         results=self._search(qstrs,'MP3')
         self._print_result(irc,results)
-    mp3=wrap(mp3,[many('something')])
+    mp3=wrap(mp3,[any('something')])
 
     def ebook(self,irc,msg,args,qstrs):
+        """[keyword]...
+        
+        Search for released ebooks.
+        """
         results=self._search(qstrs,'EBOOK')
         self._print_result(irc,results)
-    ebook=wrap(ebook,[many('something')])
+    ebook=wrap(ebook,[any('something')])
 
     def nds(self,irc,msg,args,qstrs):
+        """[keyword]...
+        
+        Search for released nds roms.
+        """
         results=self._search(qstrs,'NDS')
         self._print_result(irc,results)
-    nds=wrap(nds,[many('something')])
+    nds=wrap(nds,[any('something')])
 
     def psp(self,irc,msg,args,qstrs):
+        """[keyword]...
+        
+        Search for released psp roms.
+        """
         results=self._search(qstrs,'PSP')
         self._print_result(irc,results)
-    psp=wrap(psp,[many('something')])
+    psp=wrap(psp,[any('something')])
 
     def pda(self,irc,msg,args,qstrs):
+        """[keyword]...
+        
+        Search for released pda softwares including iphone, ipod apps.
+        """
         results=self._search(qstrs,'PDA')
         self._print_result(irc,results)
-    pda=wrap(pda,[many('something')])
+    pda=wrap(pda,[any('something')])
 
     def games(self,irc,msg,args,qstrs):
+        """[keyword]...
+        
+        Search for released pc games.
+        """
         results=self._search(qstrs,'GAMES')
         self._print_result(irc,results)
-    games=wrap(games,[many('something')])
+    games=wrap(games,[any('something')])
 
+    def console(self,irc,msg,args,qstrs):
+        """[keyword]...
+        
+        Search for released console games (including ps2, ps3, xbox, wii....).
+        """
+        results=self._search(qstrs,'CONSOLE')
+        self._print_result(irc,results)
+    console=wrap(console,[any('something')])
 
 Class = SCN
 
